f2c38d4939782d6b933ecd95c29aa99d35c9eb59
Suraj-singh04 <surajsingh877028@gmail.com>
2026-04-13 14:56:40 +0530

    fixed bugs occuring in prisma

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,22 @@
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
 
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
-
 const globalForPrisma = globalThis
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
+  prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+} else {
+  const { Pool } = require('pg')
+  const { PrismaPg } = require('@prisma/adapter-pg')
+  
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+  const adapter = new PrismaPg(pool)
+  prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+}
+
+export { prisma }
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
